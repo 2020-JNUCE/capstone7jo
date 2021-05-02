dfaee264d4cf4ac6b8d987e01d147933a524282b
shellcodesniper <shellcodesniper@icloud.com>
2021-05-02 20:41:12 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
+app.use (express.urlencoded())
+app.use (express.json());
+
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+var router = require('./router.js')(app);
+
+var server = app.listen(3000, function(){
+ console.log("Express server has started on port 3000")
+});
